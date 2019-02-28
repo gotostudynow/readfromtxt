ab089cf828ceddc726110e83e64af29929583e72
gotostudynow <1771394237@qq.com>
2019-02-28 10:18:15 +0800

    读取txt文件 xy坐标并做图

---
+++ readfromtext.py
@@ -0,0 +1,9 @@
+import scipy.signal
+import matplotlib.pyplot as plt
+import numpy as np
+data = np.loadtxt('C:/Users/mango/Desktop/test.txt')
+plt.plot(data[:,0],data[:,1])
+print(data[:,0])
+x="data[:,0]"
+y="data[:,1]"
+print(x, y)
