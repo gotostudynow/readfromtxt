f1b90d758dc562cb1111925b60fef350d8bddaec
gotostudynow <1771394237@qq.com>
2019-02-28 10:41:48 +0800

    修正了一下，x,y，简化了一下作图

--- readfromtext.py
+++ readfromtext.py
@@ -1,9 +1,13 @@
 import scipy.signal
 import matplotlib.pyplot as plt
 import numpy as np
-data = np.loadtxt('C:/Users/mango/Desktop/test.txt')
+data = np.loadtxt('C:/Users/mango/Documents/readfromtxt/test.txt')
 plt.plot(data[:,0],data[:,1])
+plt.show()
+#作图
+
 print(data[:,0])
-x="data[:,0]"
-y="data[:,1]"
+x=data[:,0]
+y=[data[:,1]]
 print(x, y)
+#加不加[]区别
